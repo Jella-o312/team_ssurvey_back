d0b81a94db6326b3a86f6c6a35087ea8d48a30db
jella <dpdnjs7708@gmail.com>
2023-11-14 22:03:20 +0900

    fun 설문 답변 뽑아오는 방법 주석으로 넣어둠

--- src/main/java/com/example/ssurvey/controller/SurveyController.java
+++ src/main/java/com/example/ssurvey/controller/SurveyController.java
@@ -1,5 +1,7 @@
 package com.example.ssurvey.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +18,7 @@ import com.example.ssurvey.domain.SurveyA;
 import com.example.ssurvey.domain.SurveyQ;
 import com.example.ssurvey.service.SurveyService;
 
-import oracle.jdbc.proxy.annotation.Post;
+
 
 @RestController
 public class SurveyController {
@@ -37,25 +39,15 @@ public class SurveyController {
 		
 		List<SurveyQ> surveyQs = surveyService.getSurveyQ(surveyNo);
 		
-		int index = 0;
-		
-		for(SurveyQ onesurveyQ : surveyQs) {
-			Integer num = onesurveyQ.getSqNo();
-			List<SurveyA> answer = surveyService.aaa(num); 
-			for(SurveyA onea : answer) {
-//				onea.getAnswer();
-				System.out.println("@@@@@" + onea.getAnswer());
-			}
-//			System.out.println("인덱스" + index + " : " +surveyService.aaa(num).get().getAnswer());
-			index++;
-		}
-		
 		
 		return new ResponseEntity<>(surveyQs, HttpStatus.OK);
 		
 	}
 	 
 	 
+	
+	
+	
 	
 	@PostMapping("/addSurvey")
 	public ResponseEntity<?> addSurvey(@RequestBody List<SurveyQ> surveyQ, String surTitle, String surveyCategory, String username) {
@@ -76,5 +68,42 @@ public class SurveyController {
 		return new ResponseEntity<>("설문 답변 완료", HttpStatus.OK);
 		
 	}
+	
+	
+	// 결과보기 눌렀을때 요청할 get endpoint 입니다. 
+//	@GetMapping("/surveyQ/{surveyNo}")
+//	public ResponseEntity<?> getSurveyQ(@PathVariable Integer surveyNo) {
+//		
+//		List<SurveyQ> surveyQs = surveyService.getSurveyQ(surveyNo);
+		// 이 위에 형식으로 동일하게 받아와야함
+	
+//	for(SurveyQ onesurveyQ : surveyQs) {
+//		Integer sqNo = onesurveyQ.getSqNo();
+//		List<String> options =  onesurveyQ.getOption(); // 객관식에서 사용됨
+//		List<String> textList = surveyService.textAnswer(sqNo);	// 단답, 장문에서 사용됨
+//			
+//		System.out.println(textList);
+//		
+//		
+//		if(onesurveyQ.getSqType() == "객관식") {
+//			
+//			int[] countAnswers = new int[options.size()];
+//
+//			for (int i = 0; i < options.size(); i++) {
+//			    String answer = options.get(i);
+//			    
+//			    // surveyService.countAnswer 메서드를 호출하여 count 값을 얻음
+//			    int countAnswer = surveyService.countAnswer(sqNo, answer);
+//			    
+//			    // 얻은 count 값을 배열에 저장
+//			    countAnswers[i] = countAnswer;
+//			}
+//		
+//		}else {
+//			
+//	
+//		}
+//		
+//	}
 	
 }
--- src/main/java/com/example/ssurvey/repository/SurveyARepository.java
+++ src/main/java/com/example/ssurvey/repository/SurveyARepository.java
@@ -3,14 +3,22 @@ package com.example.ssurvey.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.ssurvey.domain.SurveyA;
-import com.example.ssurvey.domain.SurveyQ;
 
 @Repository
 public interface SurveyARepository extends JpaRepository<SurveyA, Integer> {
 
 	public List<SurveyA> findAllBySqNo(Integer surveyNo);
+
+	    // 특정 sqNo와 answer 조건을 만족하는 레코드 개수를 세는 쿼리 메소드
+    public int countBySqNoAndAnswer(Integer sqNo, String answer);
 	
+    @Query("SELECT s.answer FROM SurveyA s WHERE s.sqNo = :sqNo")
+    List<String> findAnswersBySqNo(@Param("sqNo") Integer sqNo);
+    
+    
 }
